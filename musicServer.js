097941453e57488d65431ea67915623374ae009f
sshriyasingh <ssingh5@terpmail.umd.edu>
2025-05-14 19:53:29 -0400

    port num

--- musicServer.js
+++ musicServer.js
@@ -8,7 +8,7 @@ require("dotenv").config({
 });
 
 const app = express();
-const portNumber = process.env.PORT || 5001;
+const portNumber = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
